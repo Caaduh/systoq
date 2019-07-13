a3f10ca6c6b658d061bf7ebf1ce38bb4aa2ca598
caaduh <caaduh@gmail.com>
2019-07-13 15:45:10 -0300

    inicio do projeto

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <title>SysToq</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- NORMALIZE -->
+    <link rel="stylesheet" href="bower_components/normalize-css/normalize.css">
+
+    <!-- FONTAWESOME -->
+    <link rel="stylesheet" href="bower_components/fontAwesome/css/all.min.css">
+
+    <!-- Google Fonts -->
+    <link
+    href="https://fonts.googleapis.com/css?family=Allerta+Stencil|BioRhyme+Expanded|Fredoka+One|Great+Vibes|Kaushan+Script|Orbitron|Quattrocento|Russo+One|Special+Elite|Ultra|Lora"
+    rel="stylesheet"> 
+
+    <!-- BOOTSTRAP CORE -->
+    <link href="bower_components/MDBootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MDBOOTSTRAP -->
+    <link href="bower_components/MDBootstrap/css/mdb.min.css" rel="stylesheet">
+  </head>
+  <body>
+
+  <?php require 'nav.php'; ?>
+      
+    
+
+    <!-- SCRIPTS -->
+  <!-- JQuery -->
+  <script type="text/javascript" src="bower_components/MDBootstrap/js/jquery-3.3.1.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="bower_components/MDBootstrap/js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="bower_components/MDBootstrap/js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="bower_components/MDBootstrap/js/mdb.js"></script>
+  </body>
+</html>
---
+++ nav.php
@@ -0,0 +1,41 @@
+<nav id="navbar-responsivo" class="navbar navbar-expand-lg navbar-dark" style="padding-right: 100px; padding-left: 100px; box-sizing: border-box;">
+      <a class="navbar-brand" href="index.php" style="font-family: 'Orbitron';">Dev<span
+          style="color: rgb(8, 216, 243); font-family: 'Orbitron';">Pro</span></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a style="color: rgb(255, 255, 255); font-family: 'Roboto';"
+              class="nav-link animated bounceInLeft slow" href="index.php">Home<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a style="color: rgb(255, 255, 255); font-family: 'Roboto';"
+              class="nav-link animated bounceInLeft slow " href="sobre.php">Sobre</a>
+          </li>
+          <li class="nav-item">
+            <a style="color: rgb(255, 255, 255); font-family: 'Roboto';"
+              class="nav-link animated bounceInLeft slow " href="contato.php" tabindex="-1">Contato</a></span>
+          </li>
+          <li class="nav-item">
+            <a href="noticias.php" style=" font-family: 'Roboto';"
+              class="nav-link animated bounceInLeft slow " tabindex="-1">Notícias
+              </a></span>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <a href="entrar.php" style="color: #fff; font-family: 'Roboto';"
+            class="btn btn-outline-info my-2 my-sm-0 mr-2 d-lg-none animated bounceInLeft slow delay-1s"
+            id="btnEntrar">Entrar</a>
+
+          <a href="apoio.php" style="color: #33b5e5!important; background-color: #222!important; border-color: #33b5e5!important; font-family: 'Roboto';"
+          class="btn btn-outline-info my-2 my-sm-0 animated bounceInRight slow delay-1s delay-sm-0s">Apoiadores</a>
+
+          <a href="cadastro.php" style="color: #fff!important; background-color: #33b5e5!important; font-family: 'Roboto';"
+            class="btn btn-outline-info my-2 my-sm-0 animated bounceInRight slow delay-1s delay-sm-0s">Cadastrar</a>
+        </form>
+      </div>
+    </nav>
